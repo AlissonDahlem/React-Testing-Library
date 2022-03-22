eff9a0cabdac59c5a4bf5057c00a1a412e974028
Alisson dahlem <alissongod95@hotmail.com>
2022-03-22 17:48:49 -0300

    corrige lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import Pokedex from '../components/Pokedex';
+// import renderWithRouter from './renderWithRouter';
+// import pokemons from '../data';
+
+// describe('', () => {
+//   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+//     // renderWithRouter(<Pokedex
+//     //   pokemons={ pokemons }
+//     //   isPokemonFavoriteById={ 65 }
+//     // />);
+
+//     // const text = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
+//     // expect(text).toBeDefined();
+//   });
+// });
